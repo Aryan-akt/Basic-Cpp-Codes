6c21849f7c8d1144dd38b38ad76ca91897fa7368
Aryan <akt15oct08@gmail.com>
2026-08-21 19:31:51 +0530

    Improve comments for clarity and organization in Variables.cpp

--- Variables.cpp
+++ Variables.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 int main () {
     
+    // These are some examples of basic variables in C++
+
     int a=10, b=20;
     float pi=3.14;
     char k='A';
     bool x = false;
+
+    // And these are they're output values
+    
     cout<<"The value of a is "<<a<<".\nThe value of b is "<<b<<".\n";
     cout<<"The value of pi is "<<pi<<".";
     cout<<"\nThe value of k is "<<k<<".";
