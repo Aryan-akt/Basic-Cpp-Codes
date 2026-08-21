48122c7bce9b359be6a8cf6749fb74011d54a3da
Aryan <akt15oct08@gmail.com>
2026-08-21 19:09:05 +0530

    Add boilerplate code for initial setup and comments.

---
+++ Boilerplate Code and Comments.cpp
@@ -0,0 +1,18 @@
+/* boilerplate codes are repetitive codes required to be included in the application with little to no 
+change by the program/framework, and contribute nothing to the logic of the application.*/
+
+#include <iostream>
+
+using namespace std;
+
+// This is a comment. This does not change or affect the output of the code.
+/* A multi line comment can be written using these symbols, or by simply pressing ctrl + / after
+selecting the text */
+
+int main() {
+
+    cout << "Hello, World!";
+
+    return 0;
+
+}
