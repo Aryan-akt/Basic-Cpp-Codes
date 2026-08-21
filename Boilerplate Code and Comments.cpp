d393f2c53553c92f0bf57ee121b57f10c2d495c8
Aryan <akt15oct08@gmail.com>
2026-08-21 19:11:51 +0530

    Fix formatting and punctuation in comments for clarity

--- Boilerplate Code and Comments.cpp
+++ Boilerplate Code and Comments.cpp
@@ -1,13 +1,13 @@
 /* boilerplate codes are repetitive codes required to be included in the application with little to no 
-change by the program/framework, and contribute nothing to the logic of the application.*/
+change by the program/framework, and contribute nothing to the logic of the application. */
 
 #include <iostream>
 
 using namespace std;
 
 // This is a comment. This does not change or affect the output of the code.
-/* A multi line comment can be written using these symbols, or by simply pressing ctrl + / after
-selecting the text */
+/* A multi line comment can be written using these symbols, or by simply pressing "ctrl + /" after
+selecting the text. */
 
 int main() {
 
